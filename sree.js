d09990233e37d64f3c2951924cf09cdf1c1ca55e
Ramya Sri <ramyasree6655@gmail.com>
2025-06-24 16:08:07 +0530

    First commit

---
+++ sree.js
@@ -0,0 +1,10 @@
+// script.js
+
+function greetUser(name) {
+    const greeting = `Hello, ${name}! 👋 Welcome to the JavaScript world.`;
+    return greeting;
+}
+
+// Call the function and display the result
+const message = greetUser("Ramya");
+console.log(message);
